5de683276b30b6c608071bee174b116db0e49fc1
Gulamo <gulamo.amd@gmail.com>
2025-02-12 18:23:53 +0200

    chore: ajuste do ficheiro para frirestore apenas

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,12 +1,10 @@
 import { boot } from 'quasar'
 import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: '',
   authDomain: '',
-  dabaseURL: '',
   projectID: '',
   storagwBucket: '',
   getMessagingSenderId: '',
@@ -15,12 +13,10 @@ const firebaseConfig = {
 
 // Inicializando o Firebase
 const firebaseApp = initializeApp(firebaseConfig)
-const db = getDatabase(firebaseApp) // para realtime database
 const firestore = getFirestore(firebaseApp) // para firestore
 
-export { db, firestore }
+export { firestore }
 
 export default boot(({ app }) => {
-  app.config.globalProperties.$db = db
   app.config.globalProperties.$firestore = firestore
 })
